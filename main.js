5bf6d13d76a19ad388de08e23d36b22abbc0a9e7
VolodymyrKolomiets <kolomiietsv88@gmail.com>
2023-03-02 10:32:36 +0100

    corregido

--- main.js
+++ main.js
@@ -7,7 +7,7 @@
 // Guardar en Local Storage los datos de contacto rellenados del usuario (solo guarda un usuario)
 // Muestra el usuario que has guardado en el DOM
 // Pista: usa JSON.parse() y JSON.stringify() para guardar muchos datos usando la misma clave
-// 2. Extra
+// 2. Extra]
 // Guardar en Local Storage los datos de contacto enviados de cada usuario (ir guardandolos todos)
 // Mostrar los datos de los contactos guardados en el DOM
 // Crea un botón para borrar todos los contactos guardados en Local Storage
@@ -16,27 +16,28 @@ const nam = document.getElementById('name')
 const mail = document.getElementById('mail')
 const txt = document.getElementById('txt')
 const btn = document.getElementById('btn')
-let newUser = 0
+
 
 function sub(x) {
-    newUser = newUser+1
     x.preventDefault()
     let userDate = {Name: nam.value,
                    Email: mail.value,
                    Texto: txt.value}
-    localStorage.setItem(newUser, JSON.stringify(userDate))
-    const usuario = JSON.parse(localStorage.getItem(userDate))
+    localStorage.setItem('newUser', JSON.stringify(userDate))
+    const usuario = JSON.parse(localStorage.getItem('newUser'))
     
-    let user = document.createElement('h3')
+    let user = document.createElement('h3') //se puede poner fuera de la funcion  y se mostraria solo una linea x click
     
-    //user.textContent = `${userDate.Name} ${userDate.Email} ${userDate.Texto}`
+    //   user.textContent = `${userDate.Name} ${userDate.Email} ${userDate.Texto}`
     
-    user.innerHTML = `<p>${userDate.Name}</p> <p>${userDate.Email}</p> <p>${userDate.Texto}</p><hr>`
+    user.innerHTML = `<p>${usuario.Name}</p> <p>${usuario.Email}</p> <p>${usuario.Texto}</p><hr>`
     document.body.appendChild(user)
 }
-//let user = document.createElement('h3') //se puede poner fuera de la funcion  y se mostraria solo una linea x click
+//let user = document.createElement('h3')
 
 btn.addEventListener('click', sub)
+
+//////// Crea un botón para borrar todos los contactos guardados en Local Storage/////////
 
 const res = document.getElementById('reset')
 
